303e9491dd02246cb4c593f7cd08393e4759b51e
althe1 <alkirakosian@gmail.com>
2015-01-27 19:07:23 -0800

    Fixed logout by turning turbolinks off

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-      redirect_to mapevents_path
+      redirect_to mapevents_path, data: { no_turbolink: true }
   end
 
 end
